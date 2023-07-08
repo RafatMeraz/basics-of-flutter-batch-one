ce3e0a17f62010e1104e615eb011e78969614793
RafatMeraz <razhossain201@gmail.com>
2023-07-08 23:31:20 +0600

    hello world program added

---
+++ dart-basics/hello.dart
@@ -0,0 +1,17 @@
+main() {
+  // store value
+  // datatype variable = value;
+  String name = 'Rahim';
+  String quote = "'Who works hard!'";
+  print(quote);
+  int age = 26;
+  double amount = 300.50;
+  double bankAccountAmount = 4000.25;
+  double totalAmount = amount + bankAccountAmount;
+  bool isStudent = false;
+  print(name);
+  print(age);
+  print(amount);
+  print(totalAmount);
+  print(isStudent);
+}
